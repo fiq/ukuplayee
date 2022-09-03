a7e1b82d3e0c52f5089fb7afb522452bddac416d
Raf Gemmail <rafiq@dreamthought.com>
2022-09-04 08:11:32 +1200

    Present note in place of string/fret

--- src/components/fret.jsx
+++ src/components/fret.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Acquila from "./acquila";
 import './fret.css'
 import * as Generator  from "../generator";
+import { getNoteName } from "../generator";
 
 const Fret = (props) => {
 
+    const note = getNoteName(props["string"], props["fret"]);
     return (
         <div className="fret" onClick={()=>Generator.play(props["string"], props["fret"])}>
-            {props["string"]}---{props["fret"]}
+            {note}
             <Acquila/>
         </div>
     )
