69b862d90075147018e03868d540174189785067
Raf Gemmail <rafiq@dreamthought.com>
2022-09-18 00:41:51 +1200

    Initial move to a drawn string

--- src/components/acquila.jsx
+++ src/components/acquila.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import "./acquila.css";
 
-const Acquila = () => {
+const Acquila = (playing) => {
+    const canvasRef = useRef(null);
     console.log("acquila");
 
+    useEffect((props) => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = "black";
+        const centreX = canvas.width/2;
+        ctx.moveTo(centreX, 0)
+        ctx.lineTo(centreX, canvas.height);
+        ctx.stroke();         
+    });
     return (
-        <div className="acquila"></div>
+        <div className="acquila">
+	    <canvas ref={canvasRef}>
+		
+	    </canvas>
+	</div>
     );
 };
 
