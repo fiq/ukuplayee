3fb7858b19a821a59958bb7fdb26b37199aeb430
Raf Gemmail <rafiq@dreamthought.com>
2022-09-18 13:17:19 +1200

    Adding canvas of string prior to animation

--- src/components/acquila.jsx
+++ src/components/acquila.jsx
@@ -1,25 +1,30 @@
-import React, {useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import "./acquila.css";
 
 const Acquila = (playing) => {
+    const acquilaRef = useRef("parent");
     const canvasRef = useRef(null);
     console.log("acquila");
 
     useEffect((props) => {
-        const canvas = canvasRef.current
+        const canvas = canvasRef.current;
+        const parent = acquilaRef.current;
+
         const ctx = canvas.getContext('2d');
+        canvas.height = parent.scrollHeight;
+        console.log(parent);
+        console.debug(parent.height)
         ctx.fillStyle = "black";
-        const centreX = canvas.width/2;
-        ctx.moveTo(centreX, 0)
-        ctx.lineTo(centreX, canvas.height);
-        ctx.stroke();         
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, canvas.height);
+        ctx.lineWidth = 20;
+        ctx.stroke();
     });
     return (
-        <div className="acquila">
-	    <canvas ref={canvasRef}>
-		
-	    </canvas>
-	</div>
+        <div className="acquila" ref={acquilaRef}>
+            <canvas height="100%" className="string-canvas" ref={canvasRef}>
+            </canvas>
+        </div>
     );
 };
 
